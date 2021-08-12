746781d2c4c8d0940cec8dfaacdfd58fb13c9345
Alvaro Padilla <alvaro.padilla@bitsamericas.com>
2021-08-11 23:46:03 -0500

    Definiendo controladores del proyecto

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\OrderRepository;
+use App\Repositories\PaymentAttemptRepository;
+use App\Repositories\UserRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class RegisterController extends Controller
+{
+    /** @var UserRepository  */
+    private $userRepository;
+
+    /** @var OrderRepository  */
+    private $orderRepository;
+
+    /** @var PaymentAttemptRepository */
+    private $paymentAttemptRepository;
+
+    /**
+     * RegisterController constructor.
+     * @param UserRepository $userRepository
+     * @param OrderRepository $orderRepository
+     * @param PaymentAttemptRepository $paymentAttemptRepository
+     */
+    public function __construct(
+        UserRepository $userRepository,
+        OrderRepository $orderRepository,
+        PaymentAttemptRepository $paymentAttemptRepository
+    )
+    {
+        $this->userRepository = $userRepository;
+        $this->orderRepository = $orderRepository;
+        $this->paymentAttemptRepository = $paymentAttemptRepository;
+    }
+
+    public function show()
+    {
+        return view('auth.register');
+    }
+
+    public function store(StoreUserRequest $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            $full_name = $request->input('first_name').' '.$request->input('last_name');
+
+            /** @var User $user */
+            $user = $this->userRepository->create([
+                'name' =>  $full_name,
+                'email' =>  $request->input('email'),
+                'password' => Hash::make($request->input('password'))
+            ]);
+
+            /** @var Order $order */
+            $order = $this->orderRepository->create([
+                'customer_name' => $user->name,
+                'customer_last_name' => $request->input('last_name'),
+                'customer_email' => $user->email,
+                'customer_mobile' => $request->input('phone'),
+                'customer_document_number' => $request->input('document_number'),
+                'customer_document_type' => $request->input('document_type'),
+                'amount' => 0,
+                'status' => 'CREATED',
+                'user_id' => $user->id,
+            ]);
+
+            $this->paymentAttemptRepository->create([
+                'state' => 'INITIAL',
+                'order_id' => $order->id
+            ]);
+
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            return redirect()->back()->withErrors($exception->getMessage());
+        }
+
+        return redirect()->back()->with('alert_success', 'User Register Successful. You can now login');
+    }
+}
---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use App\Models\PaymentAttempt;
+use App\Repositories\OrderRepository;
+use App\Repositories\PaymentAttemptRepository;
+use Exception;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PaymentController extends Controller
+{
+    private $paymentAttemptRepository;
+
+    /** @var OrderRepository */
+    private $orderRepository;
+
+    /** @var PaymentService */
+    private $paymentService;
+
+    /** @var PlacetopayWebCheckoutService */
+    private $placetopayWebCheckoutService;
+
+    /**
+     * PaymentController constructor.
+     * @param PaymentAttemptRepository $paymentAttemptRepository
+     * @param OrderRepository $orderRepository
+     * @param PaymentService $paymentService
+     * @param PlacetopayWebCheckoutService $placetopayWebCheckoutService
+     */
+    public function __construct(
+        PaymentAttemptRepository $paymentAttemptRepository,
+        OrderRepository $orderRepository,
+        PaymentService $paymentService,
+        PlacetopayWebCheckoutService $placetopayWebCheckoutService
+    )
+    {
+        $this->paymentAttemptRepository = $paymentAttemptRepository;
+        $this->orderRepository = $orderRepository;
+        $this->paymentService = $paymentService;
+        $this->placetopayWebCheckoutService = $placetopayWebCheckoutService;
+    }
+
+    public function show()
+    {
+        /** @var Order $order */
+        $order = $this->orderRepository->getByUserId(Auth::user()->id);
+
+        return view('layouts.orders.pay', compact('order'));
+    }
+
+    public function process(Request $request)
+    {
+
+        try {
+            DB::beginTransaction();
+            /** @var Order $order */
+            $order = $this->orderRepository->getByUserId(Auth::user()->id);
+
+            /** @var PaymentAttempt $paymentAttemp */
+            $paymentAttempt = $order->getFirstPaymentAttempt();
+
+            $data = $this->paymentService->generateRequestData($order);
+
+            $responseCreateRequest = $this->placetopayWebCheckoutService->createRequest($data);
+            $responseGetRequestInformation = $this->placetopayWebCheckoutService->getRequestInformation($responseCreateRequest->requestId);
+
+            $this->paymentAttemptRepository->update($paymentAttempt,[
+                'external_id' => $responseCreateRequest->requestId,
+                'url_process' => $responseCreateRequest->processUrl,
+                'state' => $responseGetRequestInformation->status->status,
+                'order_id' => $order->id
+            ]);
+            DB::commit();
+
+            return redirect()->to($responseCreateRequest->processUrl);
+
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+        } catch (GuzzleException $exception) {
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+        }
+
+    }
+
+    public function updateOrderState(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+
+            /** @var Order $order */
+            $order = $this->orderRepository->getByUserId(Auth::user()->id);
+
+            /** @var PaymentAttempt $paymentAttemp */
+            $paymentAttempt = $order->getFirstPaymentAttempt();
+
+            $responseGetRequestInformation = $this->placetopayWebCheckoutService->getRequestInformation($paymentAttempt->external_id);
+
+            $this->paymentAttemptRepository->update($paymentAttempt,[
+                'external_id' => $responseGetRequestInformation->requestId,
+                'state' => $responseGetRequestInformation->status->status,
+                'order_id' => $order->id
+            ]);
+
+            $status = $responseGetRequestInformation->status->status;
+
+            if($status == 'APPROVED'){
+                $this->orderRepository->update($order, ['status' => 'PAYED']);
+            }
+
+            if($status == 'REJECTED' || $status == 'FAILED'){
+                $this->orderRepository->update($order, ['status' => 'REJECTED']);
+            }
+
+            DB::commit();
+
+            return redirect()->route('pay_order');
+
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+
+
+        } catch (GuzzleException $exception) {
+            return redirect()->route('pay_order')->withErrors(["order_error"=>"{$exception->getMessage()}"]);
+        }
+    }
+}
